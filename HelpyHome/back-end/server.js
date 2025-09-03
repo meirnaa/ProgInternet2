92a9c8dc3e9095d0a012a66406cc48bafc8902fe
meirnaa <144032242+meirnaa@users.noreply.github.com>
2025-09-03 14:51:32 -0300

    Update server.js

--- HelpyHome/back-end/server.js
+++ HelpyHome/back-end/server.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const express = require("express");
 const { Pool } = require("pg");
 const cors = require("cors");
-const allowedOrigins = ["http://127.0.0.1:5500", "http://localhost:5501", "http://localhost:3000"];
+const allowedOrigins = ["http://127.0.0.1:5500", "http://localhost:5500", "http://localhost:3000"];
 
 const app = express();
 app.use(express.json());
